211b9a70a97a1f3c4f781a8fcd9eedbca718a948
UnknownDuck <nm.brink21@gmail.com>
2022-08-26 10:46:31 -0400

    added basic print function

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Basic hello world script for using the print function
+ * 
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      System.out.println("Hello World");
+    }
+  }
+  
+  
